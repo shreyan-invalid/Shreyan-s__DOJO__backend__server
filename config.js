53ba64c08442e0594765fe2f47ea750820b60282
Shreyan Ghosh <shreyan.ghosh9@gmail.com>
2021-12-07 14:07:12 +0530

    fixed mongo

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGODB: "mongodb+srv://shreyan:12345@cluster0.gwuxi.mongodb.net/shreyan?retryWrites=true&w=majority",
+    MONGODB: "mongodb+srv://shreyan2:shreyan@shreyancluster.gwuxi.mongodb.net/prodData?retryWrites=true&w=majority",
     SECRET_KEY: 'some very secret key'
 }
